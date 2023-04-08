0fb14a0b046625969c1c9fd5f264de13ec39a9e2
Shanatta <114990082+shanatta@users.noreply.github.com>
2023-04-08 23:15:03 +0700

    Create hello_world.go

---
+++ src/hello_world.go
@@ -0,0 +1,11 @@
+//Hello World
+
+// bahasa kompleks seperti java
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("=============Hello World=============")
+	fmt.Println("hello world")
+}
